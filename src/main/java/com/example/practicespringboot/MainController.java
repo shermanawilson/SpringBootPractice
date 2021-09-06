09381ac59bbbbb4650efe466b52a71d906f2b8a5
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-09-06 09:29:40 -0500

    Update

--- src/main/java/com/example/practicespringboot/MainController.java
+++ src/main/java/com/example/practicespringboot/MainController.java
@@ -2,25 +2,18 @@ package com.example.practicespringboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.FilterConfig;
 
 
-@RestController
+
+
 @Controller
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping("")
     public String showHomePage() {
+        System.out.println("Main Controller");
         return "index";
     }
 
 
-    public void init(FilterConfig filterConfig) {
-
-    }
-
-    public void destroy() {}
 }
